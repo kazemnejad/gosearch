32b19b8d91a595f5918b5157380cbb5f0e365b40
AmirHosein KazemNejad <ub.maka@gmail.com>
2016-06-28 15:42:50 +0430

    Connect ui to backend

---
+++ gosearch/server.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from mysql.connector import MySQLConnection, conversion
+from sqlalchemy import text
+
+from flask import Flask, jsonify, render_template, request, g
+from flask_cors import CORS
+
+from gosearch.database import config
+from gosearch.database.connection import db_session
+
+app = Flask(__name__)
+app.debug = True
+app.secret_key = config.SECRET_KEY
+
+CORS(app)
+
+
+def get_db():
+    if not hasattr(g, 'db'):
+        g.db = MySQLConnection(
+            host="localhost",
+            user=config.db['user'],
+            password=config.db['password'],
+            database=config.db['dbname'],
+            autocommit=True
+        )
+
+    return g.db
+
+
+@app.route("/search", methods=["GET"])
+def search():
+    query = request.args["q"]
+    sql = '''SELECT pages.*, indexes.score, pages.*
+    FROM words
+    JOIN indexes on (words.id = indexes.word_id)
+    JOIN pages ON (indexes.page_id = pages.id)
+    WHERE words.text = "%s"
+    ORDER BY indexes.score DESC''' % conversion.MySQLConverter().escape(str(query))
+
+    cursor = get_db().cursor()
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    cursor.close()
+
+    items = []
+    for row in data:
+        items.append({
+            "id": int(row[0]),
+            "url": unicode(row[1].decode("utf-8")),
+            "title": unicode(row[2].decode("utf-8")),
+            "content": unicode(row[3][:100].decode("utf-8")),
+            "score": int(row[4])
+        })
+
+    return jsonify({"items": items})
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'db'):
+        g.db.close()
+
+
+def run():
+    global app
+    app.run()
---
+++ runserver.py
@@ -0,0 +1,3 @@
+from gosearch.server import run
+
+run()
